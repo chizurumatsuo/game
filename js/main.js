2bd75647970777882319204eec47d8f750299bf0
chizurumatsuo <48599893+chizurumatsuo@users.noreply.github.com>
2019-03-20 02:24:01 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ window.onload = function() {
 
 	var game_ = new Game(900,900); // ゲーム本体を準備すると同時に、表示される領域の大きさを設定しています。
 	game_.fps = 60; // frames(フレーム) per(毎) second(秒): ゲームの進行スピードを設定しています。
-	game_.preload('./img/chara1.png','./img/wait.png','./sound/coin03.wav','./sound/coin06.wav','./sound/coin04.wav'); // pre(前)-load(読み込み): ゲームに使う素材を予め読み込んでおきます。
+	game_.preload('./img/wait.png','./img/a.png','./sound/coin03.wav','./sound/coin06.wav','./sound/coin04.wav'); // pre(前)-load(読み込み): ゲームに使う素材を予め読み込んでおきます。
 
 	game_.onload = function() { // ゲームの準備が整ったらメインの処理を実行します。
 		var kuma = new Sprite(141, 290);  // クマというスプライト(操作可能な画像)を準備すると同時に、スプライトの表示される領域の大きさを設定しています。
