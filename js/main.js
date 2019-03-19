f34fb31cd602315f61202f180aaff61f7738149f
chizurumatsuo <48599893+chizurumatsuo@users.noreply.github.com>
2019-03-20 03:01:38 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ window.onload = function() {
 
 	var game_ = new Game(900,900); // ゲーム本体を準備すると同時に、表示される領域の大きさを設定しています。
 	game_.fps = 60; // frames(フレーム) per(毎) second(秒): ゲームの進行スピードを設定しています。
-	game_.preload('./img/wait.png','./img/a.png','./sound/coin03.wav','./sound/coin06.wav','./sound/coin04.wav'); // pre(前)-load(読み込み): ゲームに使う素材を予め読み込んでおきます。
+	game_.preload('./img/wait.png','./img/a.png','./img/kari.png','./sound/coin03.wav','./sound/coin06.wav','./sound/coin04.wav'); // pre(前)-load(読み込み): ゲームに使う素材を予め読み込んでおきます。
 
 	game_.onload = function() { // ゲームの準備が整ったらメインの処理を実行します。
 		var kuma = new Sprite(141, 290);  // クマというスプライト(操作可能な画像)を準備すると同時に、スプライトの表示される領域の大きさを設定しています。
+		var kari = new Sprite(8, 8);  // クマというスプライト(操作可能な画像)を準備すると同時に、スプライトの表示される領域の大きさを設定しています。
 		var lr=0;
 		var myLabel = new Label();
 		myLabel.font = '14px "Arial"';
@@ -20,8 +21,14 @@ window.onload = function() {
 		kuma.image = game_.assets['./img/wait.png']; // クマにあらかじめロードしておいた画像を適用します。
 		kuma.x = 100; // クマの横位置を設定します。
 		kuma.y = 120; // クマの縦位置を設定します。
+		kari.image = game_.assets['./img/kari.png']; // クマにあらかじめロードしておいた画像を適用します。
+		kari.x = 64; // クマの横位置を設定します。
+		kari.y = 64; // クマの縦位置を設定します。
+		kari.scaleX = 8;
+		kari.scaleY = 8;
 		game_.rootScene.addChild(myLabel);
 		game_.rootScene.addChild(kuma); // ゲームのシーンにクマを表示させます。
+		game_.rootScene.addChild(kari); // ゲームのシーンにクマを表示させます。
 		game_.rootScene.backgroundColor  = '#7ecef4'; // ゲームの動作部分の背景色を設定しています(16進数)。
 		var speed = 1;// クマのスピードを表す変数(ハコ)を用意しておきます。
 
