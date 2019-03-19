bbca22da4444f652752485fd7131ee9ec934ef86
chizurumatsuo <48599893+chizurumatsuo@users.noreply.github.com>
2019-03-20 00:49:41 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,20 +4,20 @@ window.onload = function() {
 
     // 行の終わりには、;(セミコロン)をつけます。
 
-	var game_ = new Game(); // ゲーム本体を準備すると同時に、表示される領域の大きさを設定しています。
+	var game_ = new Game(900,900); // ゲーム本体を準備すると同時に、表示される領域の大きさを設定しています。
 	game_.fps = 60; // frames(フレーム) per(毎) second(秒): ゲームの進行スピードを設定しています。
-	game_.preload('./img/chara1.png','./img/chara2.png','./sound/coin03.wav','./sound/coin06.wav','./sound/coin04.wav'); // pre(前)-load(読み込み): ゲームに使う素材を予め読み込んでおきます。
+	game_.preload('./img/chara1.png','./img/wait.png','./sound/coin03.wav','./sound/coin06.wav','./sound/coin04.wav'); // pre(前)-load(読み込み): ゲームに使う素材を予め読み込んでおきます。
 
 	game_.onload = function() { // ゲームの準備が整ったらメインの処理を実行します。
-		var kuma = new Sprite(32, 32);  // クマというスプライト(操作可能な画像)を準備すると同時に、スプライトの表示される領域の大きさを設定しています。
+		var kuma = new Sprite(141, 290);  // クマというスプライト(操作可能な画像)を準備すると同時に、スプライトの表示される領域の大きさを設定しています。
 		var lr=0;
-		var myLabel = new Label(lr);
-		myLabel.font = "16px Palatino";
+		var myLabel = new Label();
+		myLabel.font = '14px "Arial"';
 		myLabel.x = 32;	// X座標
 		myLabel.y = 32;	// Y座標
-		var posx=0;
-		var posy=0;
-		kuma.image = game_.assets['./img/chara2.png']; // クマにあらかじめロードしておいた画像を適用します。
+		var posx;
+		var posy;
+		kuma.image = game_.assets['./img/wait.png']; // クマにあらかじめロードしておいた画像を適用します。
 		kuma.x = 100; // クマの横位置を設定します。
 		kuma.y = 120; // クマの縦位置を設定します。
 		game_.rootScene.addChild(myLabel);
@@ -45,14 +45,16 @@ window.onload = function() {
 		}
 	});
         game_.rootScene.addEventListener(Event.TOUCH_MOVE, function(e) {
-		if (e.x > posx && lr!='L') {
-			posx=e.x;
-			lr='L';
-			var sounda = game_.assets['./sound/coin06.wav'].clone();
-			sounda.play();
-		} else if(e.x < posx && lr!='R'){ 
+		if (e.x > posx && lr!='R') {
 			posx=e.x;
 			lr='R';
+			myLabel.text = lr;
+			var sounda = game_.assets['./sound/coin06.wav'].clone();
+			sounda.play();
+		} else if(e.x < posx && lr!='L'){ 
+			posx=e.x;
+			lr='L';
+			myLabel.text = lr;
 			var soundb = game_.assets['./sound/coin04.wav'].clone();
 			soundb.play();
 
@@ -61,6 +63,7 @@ window.onload = function() {
         game_.rootScene.addEventListener(Event.TOUCH_END, function(e) {
 		posx=e.x;
 		lr='';
+		myLabel.text = lr;
 	});
 
 }
